9d97dcb159fce782bb29d5e59594f0c82df0092e
Andrew Browne <dersaidin@dersaidin.net>
2012-01-22 21:10:19 +1000

    Updated windows map compile scripts.

--- maps/bow_BLOCK.bat
+++ maps/bow_BLOCK.bat
@@ -1,1 +1,3 @@
-start compile.bat bow_BLOCK
+SET compile_bat=%~dp0\..\..\maps\compile.bat
+
+start %compile_bat% bow_BLOCK
---
+++ maps/bow_BLOCK_meta.bat
@@ -0,0 +1,3 @@
+SET compile_bat=%~dp0\..\..\maps\compile_meta.bat
+
+start %compile_bat% bow_BLOCK
